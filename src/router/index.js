9ec6aeffaa0a076ff1abe34cdad1d085cefc2f1f
Muhammetnur22 <bagbekovmuhammetnur22@gmail.com>
2022-07-19 17:44:53 +0500

    final

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,12 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Eventlist from "../views/EventList.vue";
-import EventDetails from "../views/EventDetails.vue"
+import EventLayout from "../views/event/LayouT.vue"
+import EventDetails from "../views/event/DetailS.vue"
+import EventRegister from "../views/event/RegisteR.vue"
+import EventEdit from "../views/event/EdiT.vue"
 import About from "../views/AboutT.vue";
+import NotFound from "../views/NotFound.vue";
+import NetworkError from "../views/NetworkError.vue";
 
 
 const routes = [
@@ -9,18 +14,60 @@ const routes = [
     path: "/",
     name: "EventList",
     component: Eventlist,
+    props: route => ({ page: parseInt(route.query.page) || 1})
   },
   {
-    path: "/event/:id",
-    name: "EventDetails", 
+    path: "/events/:id",
+    name: "EventLayout", 
     props: true,
-    component: EventDetails
+    component: EventLayout,
+    children: [
+      {
+        path: "",
+        name: "EventDetails", 
+        component: EventDetails
+      },
+      {
+        path: "register",
+        name: "EventRegister", 
+        component: EventRegister
+      },
+      {
+        path: "edit",
+        name: "EventEdit", 
+        component: EventEdit
+      },
+    ]
   },
   {
-    path: "/about",
-    name: "About", 
-    component: About
+    path: "/event/:afterEvent(.*)",
+    redirect: to =>{
+      return { path: '/events/'+ to.params.afterEvent}
+    }
   },
+  {
+    path: "/about-us",
+    name: "About", 
+    component: About,
+    alias: '/about'
+  },
+  {
+    path: '/:catchAll(.*)',
+    name: 'NotFound',
+    component: NotFound
+  },
+  {
+    path: '/404/:resource',
+    name: '404Resource',
+    component: NotFound,
+    props: true
+  },
+  {
+    path: '/network-error',
+    name: 'NetworkError',
+    component: NetworkError,
+  }
+  
 ];
 
 const router = createRouter({
--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -1,34 +1,83 @@
 <template>
   <h1>Events for Good</h1>
-    <div class="events">
-      <EventCard v-for="event in events" :key="event.id" :event="event"/>
+  <div class="events">
+    <EventCard v-for="event in events" :key="event.id" :event="event" />
+
+    <div class="pagination">
+      <router-link
+        id="page-prev"
+        :to="{ name: 'EventList', query: { page: page - 1 } }"
+        rel="prev"
+        v-if="page != 1"
+        >&#60; Previous</router-link
+      >
+
+      <router-link
+        id="page-next"
+        :to="{ name: 'EventList', query: { page: page + 1 } }"
+        rel="next"
+        v-if="hasNextPage"
+        >Next &#62;</router-link
+      >
     </div>
+  </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import EventCard from "@/components/EventCard.vue";
+import EventCard from '@/components/EventCard.vue'
 import EventService from '@/services/EventService.js'
+import NProgess from 'nprogress'
 
 export default {
-  name: "EventList",
+  name: 'EventList',
+  props: ['page'],
   components: {
-    EventCard,
+    EventCard
   },
-  data(){
+  data() {
     return {
-      events: null
+      events: null,
+      totalEvents: 0
     }
   },
-  created(){
-    EventService.getEvents()
-      .then(response =>{
-        this.events = response.data
+
+  beforeRouteEnter(routeTo, routeFrom, next){
+    NProgess.start()
+    EventService.getEvents(2, parseInt(routeTo.query.page) || 1)
+      .then(response => {
+        next(comp =>{
+          comp.events = response.data
+          comp.totalEvents = response.headers['x-total-count']
+        })
       })
-      .catch(error => console.log(error)
-      )
+      .catch(() => {
+        next({name: 'NetworkError'})
+      })
+      .finally(()=>{
+        NProgess.done()
+      })
+  },
+  beforeRouteUpdate(routeTo){
+    NProgess.start()
+    EventService.getEvents(2, parseInt(routeTo.query.page) || 1)
+      .then(response => {
+          this.events = response.data
+          this.totalEvents = response.headers['x-total-count']
+      })
+      .catch(() => {
+        return { name: 'NetworkError' }
+      })
+      .finally(()=>{
+        NProgess.done()
+      })
+    },
+  computed: {
+    hasNextPage() {
+      var totalPages = Math.ceil(this.totalEvents / 2)
+      return this.page < totalPages
+    }
   }
-};
+}
 </script>
 
 <style scoped>
@@ -36,5 +85,20 @@ export default {
   display: flex;
   flex-direction: column;
   align-items: center;
+}
+.pagination {
+  display: flex;
+  width: 290px;
+}
+.pagination a {
+  flex: 1;
+  text-decoration: none;
+  color: #2c3e50;
+}
+#page-prev {
+  text-align: left;
+}
+#page-next {
+  text-align: right;
 }
 </style>
